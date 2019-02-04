abc9729c9c1ca6a0dedbb6720ca794d95e73c888
Searjasub Lopez <searjasub.lopez@outlook.com>
2019-02-04 10:04:46 -0700

    instance of clinic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,9 @@
+import view.UserInteraction;
+
 public class Main {
+
+    public static void main(String[] args) {
+        ClinicController clinic = new ClinicController(new UserInteraction());
+
+    }
 }
