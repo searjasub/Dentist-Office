64a4f9824bf29a6b0c7c25a4184beb6e8c557308
Searjasub Lopez <searjasub.lopez@outlook.com>
2019-02-07 22:50:47 -0700

    Catched some exception in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,14 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        ClinicController clinic = new ClinicController(new UserInteraction());
-        clinic.start();
+    public static void main(String[] args) {
+        try {
+            ClinicController clinic = new ClinicController(new UserInteraction());
+            clinic.start();
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Classes got mixed and couldn't find the right one.");
+        } catch (IOException ex){
+            System.out.println("If you see this, that means that the ConsoleUI is not working.");
+        }
     }
 }
